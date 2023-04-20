61a05fd5c0c46859a3fa83afbafe4c02a9f06c67
Alex Geovanni <A180065@unach.mx>
2023-04-19 20:17:03 -0600

    casi listo

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,29 +1,37 @@
 const abrir_nav = document.querySelector("#abrir-nav");
 const cerrar_nav = document.querySelector("#cerrar-nav");
+const nav = document.querySelector("[data-nav]");
+const a = nav.querySelectorAll('a')
 
-
-abrir_nav.addEventListener('click',(e)=>{
-    let hamb = e.target;
-    hamb.style.opacity="0";
-    hamb.style.display= "none"
-
+abrir_nav.addEventListener('click',()=>{
+    abrir_nav.style.opacity="0";
+    abrir_nav.style.display= "none"
     cerrar_nav.style.opacity="1";
     cerrar_nav.style.display= "block"
     cerrar_nav.style.color="#fff"
     menu();
 })
 
-cerrar_nav.addEventListener('click', (e)=>{
-    let cerrar = e.target;
+cerrar_nav.addEventListener('click', ()=>{
     abrir_nav.style.opacity="1";
     abrir_nav.style.display= "block"
-
-    cerrar.style.opacity="0";
-    cerrar.style.display= "none"
+    cerrar_nav.style.opacity="0";
+    cerrar_nav.style.display= "none"
     menu();
-
 })
-function menu(e){
-    const nav = document.querySelector("[data-nav]");
-    nav.classList.toggle("abrir__nav")
+
+a.forEach((Element)=>{
+    Element.addEventListener('click',()=>{
+        abrir_nav.style.opacity="1";
+        abrir_nav.style.display= "block"
+        cerrar_nav.style.opacity="0";
+        cerrar_nav.style.display= "none"
+        menu();
+    })
+})
+
+
+
+function menu(){
+    nav.classList.toggle("abrir__nav");
 }
