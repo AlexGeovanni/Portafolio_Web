e05df8ecca606963da8a38b703a965e28ab8b483
Alex Geovanni <A180065@unach.mx>
2023-06-02 17:40:16 -0600

    Actualizando doc

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
-const form = document.querySelector('[data-formContacto]');
-const Btn = document.querySelector('[data-button]');
-
-form.addEventListener('submit',(e)=>{
+    const form = document.querySelector('[data-formContacto]');
+    form.addEventListener('submit',(e)=>{
     e.preventDefault();
+    const Nombre = document.querySelector('#nombre').value;
+    const Email = document.querySelector('#email').value;
+    const Msj = document.querySelector('#mensaje').value;
+    window.location.href=`mailto:geovanialex8@gmail.com?subject=envioDesdeFormulario&body=Nombre%3A${Nombre}%0ACorrreo%3A${Email}%0AMensaje%3A${Msj}`
 })
+
