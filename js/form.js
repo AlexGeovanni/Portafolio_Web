db909c8ef2e0a2db9935525baf9aa7c5d9e3ca01
Alex Geovanni <A180065@unach.mx>
2023-06-02 15:37:11 -0600

    Actualizando doc

---
+++ js/form.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('[data-formContacto]');
+const Btn = document.querySelector('[data-button]');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+})
